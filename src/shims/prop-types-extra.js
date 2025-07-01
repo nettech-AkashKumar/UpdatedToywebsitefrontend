f24c5e2d6e5c3b2c123c20b070fca862598cec87
nettech-AkashKumar <akash.kumar@netarioinnovations.com>
2025-07-01 11:50:25 +0530

    sixth commit

---
+++ src/shims/prop-types-extra.js
@@ -0,0 +1,2 @@
+// src/shims/prop-types-extra.js
+export default {};
--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,32 @@
 
 
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [tailwindcss(), react()],
+//   resolve: {
+//     extensions: ['.jsx', '.json'],
+//     alias: {
+//       'react-smooth': path.resolve(__dirname, './react-smooth-fix.js'),
+//        // NEW FIX for build failure due to missing module in react-bootstrap
+//       'prop-types-extra/lib/all': path.resolve(__dirname, './src/shims/prop-types-extra.js'),
+//     },
+//   },
+//   optimizeDeps: {
+//     include: [
+//       "recharts",
+//       '@popperjs/core/lib/modifiers/arrow',
+//     ],
+//   },
+// })
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -32,6 +58,19 @@ export default defineConfig({
     },
   },
   optimizeDeps: {
-    include: ["recharts"],
+    include: [
+      'recharts',
+      '@popperjs/core',
+      '@popperjs/core/lib/modifiers/arrow',
+      '@popperjs/core/lib/modifiers/computeStyles', //  Added this too
+    ],
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        '@popperjs/core/lib/modifiers/arrow',
+        '@popperjs/core/lib/modifiers/computeStyles', // Externalize it too
+      ],
+    },
   },
 })
