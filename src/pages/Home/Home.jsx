587ea75e757f411e7fcb013e32d3ce52d01d70b0
nettech-AkashKumar <akash.kumar@netarioinnovations.com>
2025-07-01 12:36:52 +0530

    import problemss

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PageLayout from '../../layouts/PageLayout';
 import ElevateSection from '../../components/HomeComponents/ElevateSection/ElevateSection';
-import Cardcarousal from '../../components/HomeComponents/CardCarousal/CardCarousal.jsx'
+import Cardcarousal from '../components/HomeComponents/CardCarousal/CardCarousal.jsx'
 import Knockoutoffer from '../../components/HomeComponents/Knockoutoffer/Knockoutoffer';
 import CardOffer from '../../components/HomeComponents/cardoffer/cardoffer';
 import ShopByCateg from '../../components/HomeComponents/shopbycategoryheading/ShopByCateg';
